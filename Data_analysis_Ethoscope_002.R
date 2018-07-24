f082fbefc111d3d8ba141756cbb9666112ceb943
thinkerahead <thinkerahead@users.noreply.github.com>
2018-07-24 19:00:17 +0200

    Update Data_analysis_Ethoscope_002.R
    added counting interactions.

--- Data_analysis_Ethoscope_002.R
+++ Data_analysis_Ethoscope_002.R
@@ -30,6 +30,18 @@ ggetho(dt_curated, aes(y=asleep, colour=sex), time_wrap = hours(24)) +
   stat_ld_annotations() +
   facet_grid(Temperature ~ .) +
   scale_y_continuous(name= "Fraction of time sleeping",labels = scales::percent)
+
+# Create plot for sum of interactions (rotations) for each fly every 30 min
+#colour could be changed to sex, genotype, etc. summary_FUN could be sum, mean, etc, summary_time_window could be any time intervals
+
+ggetho(dt_curated, aes(y=interactions, colour=background), time_wrap = hours(24), summary_FUN = sum, summary_time_window = mins(30))+
+  stat_pop_etho(mapping = NULL, data = NULL, geom = "smooth", position = "identity", method = mean_se, method.args = list(),
+                show.legend = NA, inherit.aes = TRUE)+
+  stat_ld_annotations(mapping = NULL, data = NULL, position = "identity", ld_colours = c("white", "black"), ypos = "bottom", 
+                      height = 0.03, period = hours(24), phase = 0, l_duration = hours(12), outline = "black", 
+                      x_limits = c(NA, NA), na.rm = FALSE, show.legend = TRUE, inherit.aes = TRUE)
+
+
 ### above function can be used for different plotting
 -------------------------------------------------------------------------------------------------------
 # plotting sunmmery_dt file, sleep fraction during night time, day time or total sleep 
